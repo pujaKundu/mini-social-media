c0c40bc1d315dc81d07cbd1060606ac38d70aa0f
pujaKundu <iampujakundu@gmail.com>
2022-06-25 01:29:46 +0600

    date,time,location added

--- main.js
+++ main.js
@@ -7,24 +7,90 @@ const showPost = document.getElementById("show-post");
 //showPost.textContent = postContent.value;
 
 const allPosts = document.getElementById('all-posts')
-let c = 0;
+
+let c = 1;
+
+//date time
+
+const today = new Date();
+
+const date =
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+const time =
+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+//new post
 postBtn.addEventListener('click', () => {
     
-
     let newPost = postContent.value;
     
     allPosts.innerHTML += `
         <div id="new-post" class='newPost'>
             <p id="show-post">${newPost}</p>
-            <small>Likes: ${c}</small>
-            <button id='likeHandler'>Like</button>
+            <div class='likeContainer'>
+            <button id="like-btn">Like</button>
+            <div id='like'></div>
+            
+            </div>
+            <small>Date: ${date} <span>Time: ${time}</span></small>
+            
         </div>
     `;
-    postContent.value = "";
-   // console.log(postContent.value);
+  
+  postContent.value = "";
+  const like = document.getElementById("like-btn");
+  const showLike = document.getElementById('like')
+  like.addEventListener("click", inc);
+  function inc() {
+    console.log("d");
+    showLike.innerHTML = `<span id="likes"><i class="material-icons">favorite</i> ${c++}</span>
+    `;
+  }
         
 })
 
+
+const  locateMe =()=> {
+  const status = document.querySelector("#status");
+  const mapLink = document.querySelector("#map-link");
+
+  mapLink.href = "";
+  mapLink.textContent = "";
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = "";
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+
+  if (!navigator.geolocation) {
+    status.textContent = "Geolocation is not supported by your browser";
+  } else {
+    navigator.geolocation.getCurrentPosition(success, console.log);
+  }
+}
+locateMe();
+//document.querySelector("#find-me").addEventListener("onload", geoFindMe);
+
+
+// const successfulLookup = position => {
+//   const { latitude, longitude } = position.coords;
+//   fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=1234`)
+//     .then(response => response.json()
+//       .then(console.log))
+// }
+// window.navigator.geolocation.getCurrentPosition(successfulLookup, console.log);
+    //locationTag.innerHTML = ``
+
+
+//window.navigator.geolocation.getCurrentPosition(console.log, console.log);
+
+
+//document.getElementById("like-btn").addEventListener("click", inc);
 //video
 const uploadVideo = document.getElementById("uploadedVideo");
 
